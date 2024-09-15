fe46ebd1cd5e6c3e8bac96a7b8b133333c5b1893
Dajlan Zaimaj <51968936+dailani@users.noreply.github.com>
2024-09-15 16:09:45 +0200

    add raydium demo

---
+++ raydium-sdk-V2-demo/src/amm/createMarket.ts
@@ -0,0 +1,56 @@
+import { RAYMint, USDCMint, OPEN_BOOK_PROGRAM, DEVNET_PROGRAM_ID, WSOLMint } from '@raydium-io/raydium-sdk-v2'
+import { initSdk, txVersion } from '../config'
+
+export const createMarket = async () => {
+  const raydium = await initSdk()
+
+  // check mint info here: https://api-v3.raydium.io/mint/list
+  // or get mint info by api: await raydium.token.getTokenInfo('mint address')
+
+  const { execute, extInfo, transactions } = await raydium.marketV2.create({
+    baseInfo: {
+      mint: RAYMint,
+      decimals: 6,
+    },
+    quoteInfo: {
+      mint: USDCMint,
+      decimals: 9,
+    },
+    lotSize: 1,
+    tickSize: 0.01,
+    dexProgramId: OPEN_BOOK_PROGRAM,
+    // dexProgramId: DEVNET_PROGRAM_ID.OPENBOOK_MARKET, // devnet
+
+    // requestQueueSpace: 5120 + 12, // optional
+    // eventQueueSpace: 262144 + 12, // optional
+    // orderbookQueueSpace: 65536 + 12, // optional
+
+    txVersion,
+    // optional: set up priority fee here
+    // computeBudgetConfig: {
+    //   units: 600000,
+    //   microLamports: 100000000,
+    // },
+  })
+
+  console.log(
+    `create market total ${transactions.length} txs, market info: `,
+    Object.keys(extInfo.address).reduce(
+      (acc, cur) => ({
+        ...acc,
+        [cur]: extInfo.address[cur as keyof typeof extInfo.address].toBase58(),
+      }),
+      {}
+    )
+  )
+
+  const txIds = await execute({
+    // set sequentially to true means tx will be sent when previous one confirmed
+    sequentially: true,
+  })
+
+  console.log('create market txIds:', txIds)
+}
+
+/** uncomment code below to execute */
+// createMarket()
---
+++ raydium-sdk-V2-demo/src/farm/unstake.ts
@@ -0,0 +1,30 @@
+import { initSdk, txVersion } from '../config'
+import BN from 'bn.js'
+
+export const unstake = async () => {
+  const raydium = await initSdk()
+  const targetFarm = 'CHYrUBX2RKX8iBg7gYTkccoGNBzP44LdaazMHCLcdEgS' // RAY-USDC farm
+
+  // note: api doesn't support get devnet farm info
+  const farmInfo = (await raydium.api.fetchFarmInfoById({ ids: targetFarm }))[0]
+
+  const readyUnStakeAmount = new BN(100)
+
+  const { execute } = await raydium.farm.withdraw({
+    farmInfo,
+    amount: readyUnStakeAmount,
+    txVersion,
+    // optional: set up priority fee here
+    // computeBudgetConfig: {
+    //   units: 600000,
+    //   microLamports: 100000000,
+    // },
+  })
+
+  // don't want to wait confirm, set sendAndConfirm to false or don't pass any params to execute
+  const { txId } = await execute({ sendAndConfirm: true })
+  console.log('farm staked:', { txId: `https://explorer.solana.com/tx/${txId}` })
+}
+
+/** uncomment code below to execute */
+// unstake()
